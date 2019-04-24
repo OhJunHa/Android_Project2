2b50dbdc0bd8d25358c9e41f02b9a3be1a12d79e
OhJunha <qpfmrk2004@gmail.com>
2019-04-24 16:48:02 +0900

    19.04.24

--- app/src/main/java/com/example/user/android_project2/MapActivity.java
+++ app/src/main/java/com/example/user/android_project2/MapActivity.java
@@ -2,9 +2,13 @@ package com.example.user.android_project2;
 
 
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -16,14 +20,47 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 
 
+
+
 @SuppressWarnings("ALL")
+
 public class MapActivity extends AppCompatActivity
         implements OnMapReadyCallback {
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        Button button1 =(Button) findViewById(R.id.bt01);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ListviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button button2 =(Button) findViewById(R.id.bt02);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button3 =(Button) findViewById(R.id.bt03);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),HelpActivity.class);
+                startActivity(intent);
+            }
+        });
 
         FragmentManager fragmentManager = getFragmentManager();
         MapFragment mapFragment = (MapFragment)fragmentManager
